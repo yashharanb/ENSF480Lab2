9032ef84cea100fa9364832835aa195bb3446e29
yashharanb <37969217+yashharanb@users.noreply.github.com>
2018-09-21 23:27:49 -0600

    Add files via upload

---
+++ graphicsWorld.h
@@ -0,0 +1,11 @@
+// graphicsWorld.h
+// ENSF 480 - Lab 2
+// By: Nicole Caballero and Yashharan Babra
+
+#ifndef GRAPHICSWORLD_H
+#define GRAPHICSWORLD_H
+class GraphicsWorld {
+ public:
+	void run();
+};
+#endif
