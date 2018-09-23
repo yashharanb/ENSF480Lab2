ede20578ef439bbfe416b3c2db5a0e087d24f7d0
nicole-ec <nicole.caballero@ucalgary.ca>
2018-09-23 09:37:29 -0600

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+void Square::display(){
+	
+	
+}
+
+double Square::getArea(){
+	
+	
+}
+
+double Square::getPerimeter(){
+	
+	
+}	
+
+int Square::getSideA(){
+	
+}
+		
+int Square::setSideA(){
+	
+}
+		
+void Square::display(){
+	
+}
+		
